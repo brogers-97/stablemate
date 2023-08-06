36920102c13bd513cb153605d5b4954e7c47b51a
brogers-97 <brburr24@gmail.com>
2023-08-05 23:09:53 -0600

    added basic frontend components

---
+++ src/components/HorseInfo.jsx
@@ -0,0 +1,7 @@
+export default function HorseInfo() {
+    return(
+        <div>
+            <h1>Horses individual info page</h1>
+        </div>
+    )
+}
---
+++ src/components/HorseSearch.jsx
@@ -0,0 +1,7 @@
+export default function HomeSearch() {
+    return(
+        <div>
+            <h1>Search through horses</h1>
+        </div>
+    )
+}

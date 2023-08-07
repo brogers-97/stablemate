6b49f43d9896a8fd39768e51b3e6d60c40337211
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-08-07 12:47:58 -0600

    adds dummy horse data.js, adds some search function to horseSearch component

--- src/components/HorseSearch.jsx
+++ src/components/HorseSearch.jsx
@@ -1,7 +1,49 @@
+import { useState } from 'react'
+import { horses } from '../data/data.js'
+import "./horseSearch.css"
+
 export default function HorseSearch() {
+
+    const [searchTerm, setSearchTerm] = useState('')
+    const [filteredHorses, setFilteredHorses] = useState(horses)
+
+    const handleInputChange = (e) => {
+        const value = e.target.value
+        setSearchTerm(value)
+
+        const filtered = horses.filter(horse => {
+            return horse.name.toLowerCase().includes(value.toLowerCase())
+        })
+
+        setFilteredHorses(filtered)
+    }
+
     return(
-        <div>
+        <main>
             <h1>Search through horses</h1>
-        </div>
+
+            <section className='search-container'>
+                <input 
+                    type='text'
+                    placeholder="Search horse's name"
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                />
+
+            </section>
+
+            <section className='horses-list'>
+                {filteredHorses.map((horse, index) => (
+                    <div key={index}>
+                        <h2>{horse.name}</h2>
+                        <p>Gender: {horse.gender}</p>
+                        <p>Breed: {horse.breed}</p>
+                        <p>Age: {horse.age}</p>
+                        {horse.gender === 'Female' && <p>Pregnancy Status: {horse.pregnancyStatus ? 'Pregnant' : 'Not Pregnant'}</p>}
+                    </div>
+                ))}
+            </section>
+
+        </main>
     )
 }
---
+++ src/data/data.js
@@ -0,0 +1,13 @@
+// dummy data
+export const horses = [
+    {name: '3030', gender: 'Male', breed: 'Thoroughbred', age: 7, pregnancyStatus: false},
+    {name: 'Rose', gender: 'Female', breed: 'Mustang', age: 5, pregnancyStatus: true},
+    {name: 'Twist', gender: 'Female', breed: 'Arabian', age: 9, pregnancyStatus: false},
+    {name: 'Daisy', gender: 'Female', breed: 'Clydesdale', age: 6, pregnancyStatus: false},
+    {name: 'Outfoxxed', gender: 'Male', breed: 'Pinto', age: 8, pregnancyStatus: false},
+    {name: 'Faith', gender: 'Female', breed: 'Andalusian', age: 7, pregnancyStatus: true},
+    {name: 'Grace', gender: 'Female', breed: 'Friesian', age: 4, pregnancyStatus: false},
+    {name: 'Hope', gender: 'Female', breed: 'Appaloosa', age: 10, pregnancyStatus: true},
+    {name: 'Iggy', gender: 'Male', breed: 'Quarter Horse', age: 3, pregnancyStatus: false},
+    {name: 'Jack', gender: 'Male', breed: 'Tennessee Walker', age: 6, pregnancyStatus: false}
+    ];

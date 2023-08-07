03bc4df6bc157486c98be400df3309dc751c19d5
Garrett Heiner <64627881+Heiner000@users.noreply.github.com>
2023-08-07 12:03:35 -0600

    adds calendar and horses links to navbar header

--- src/components/HorseSearch.jsx
+++ src/components/HorseSearch.jsx
@@ -1,4 +1,4 @@
-export default function HomeSearch() {
+export default function HorseSearch() {
     return(
         <div>
             <h1>Search through horses</h1>
--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -1,10 +1,21 @@
+import { Link } from 'react-router-dom'
 import './header.css'
 
 export default function Header() {
     return(
         <div className='header'>
             {/* will need to change to users ranch name */}
+            <Link to='/'>
             <h1>KK Ranch</h1>
+            </Link>
+
+            <Link to='/calendar'>
+                <span>Calendar</span>
+            </Link>
+
+            <Link to='/horseSearch'>
+                <span>Horses</span>
+            </Link>
         </div>
     )
 }
